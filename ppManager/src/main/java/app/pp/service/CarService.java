619a713689060152704bf67c8de1e5018bad6b0c
lzz <1353508760@qq.com>
2019-04-22 15:51:05 +0800

    品牌车辆相关接口

---
+++ ppManager/src/main/java/app/pp/service/CarService.java
@@ -0,0 +1,28 @@
+package app.pp.service;
+
+import app.pp.common.Result;
+import app.pp.entity.Car;
+
+import java.util.List;
+
+//品牌车系相关接口
+public interface CarService {
+
+    //添加品牌或车系
+    public Result save(Car car);
+
+    //删除品牌或车系
+    public Result del(Integer id);
+
+    //删除车系
+    public Result dels(List<Integer> ids);
+
+    //更新品牌或车系
+    public Result update(Car car);
+
+    //查询牌品或车系详情
+    public Result info(Integer id);
+
+    //品牌车系列表、
+    public Result list(Integer fid,Integer page);
+}
