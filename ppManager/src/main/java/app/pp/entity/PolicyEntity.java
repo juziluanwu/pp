81e05977eaa46b7343b173163b2f31a1249b07f5
zhw <782265504@qq.com>
2020-05-23 19:23:25 +0800

    车保

--- ppManager/src/main/java/app/pp/entity/PolicyEntity.java
+++ ppManager/src/main/java/app/pp/entity/PolicyEntity.java
@@ -1,8 +1,12 @@
 package app.pp.entity;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
+@Data
 public class PolicyEntity {
 
 
@@ -40,139 +44,9 @@ public class PolicyEntity {
 
     private String salenum;
 
-    public String getCarname() {
-        return carname;
-    }
+    private BigDecimal amount1;
 
-    public void setCarname(String carname) {
-        this.carname = carname;
-    }
+    private BigDecimal amount2;
 
-    public List<Group> getGroup() {
-        return group;
-    }
-
-    public void setGroup(List<Group> group) {
-        this.group = group;
-    }
-
-    public Integer getPage() {
-        return page;
-    }
-
-    public void setPage(Integer page) {
-        this.page = page;
-    }
-
-    public Integer getBuycartype() {
-        return buycartype;
-    }
-
-    public void setBuycartype(Integer buycartype) {
-        this.buycartype = buycartype;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getPnum() {
-        return pnum;
-    }
-
-    public void setPnum(String pnum) {
-        this.pnum = pnum;
-    }
-
-    public Integer getState() {
-        return state;
-    }
-
-    public void setState(Integer state) {
-        this.state = state;
-    }
-
-    public Integer getSaleid() {
-        return saleid;
-    }
-
-    public void setSaleid(Integer saleid) {
-        this.saleid = saleid;
-    }
-
-    public Integer getGroupid() {
-        return groupid;
-    }
-
-    public void setGroupid(Integer groupid) {
-        this.groupid = groupid;
-    }
-
-    public Integer getIsdel() {
-        return isdel;
-    }
-
-    public void setIsdel(Integer isdel) {
-        this.isdel = isdel;
-    }
-
-    public Date getCreatedtime() {
-        return createdtime;
-    }
-
-    public void setCreatedtime(Date createdtime) {
-        this.createdtime = createdtime;
-    }
-
-    public Long getCreator() {
-        return creator;
-    }
-
-    public void setCreator(Long creator) {
-        this.creator = creator;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
-    }
-
-    public Integer getNumber() {
-        return number;
-    }
-
-    public void setNumber(Integer number) {
-        this.number = number;
-    }
-
-    public Integer getVerb() {
-        return verb;
-    }
-
-    public void setVerb(Integer verb) {
-        this.verb = verb;
-    }
-
-    public String getGroupname() {
-        return groupname;
-    }
-
-    public void setGroupname(String groupname) {
-        this.groupname = groupname;
-    }
-
-    public String getSalenum() {
-        return salenum;
-    }
-
-    public void setSalenum(String salenum) {
-        this.salenum = salenum;
-    }
+    private String yearlimit;
 }
